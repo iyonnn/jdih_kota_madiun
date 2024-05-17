8e5d74445d44bbd34b25e58ad73a8e4fe915ee9b
iyonnn <rochmadekocahyono93@gmail.com>
2024-05-17 11:34:30 +0700

    first commit

---
+++ lib/main.dart
@@ -0,0 +1,28 @@
+import 'package:animated_splash_screen/animated_splash_screen.dart';
+import 'package:flutter/material.dart';
+import 'package:jdih_kota_madiun/home.dart';
+import 'package:page_transition/page_transition.dart';
+
+void main() {
+  WidgetsFlutterBinding.ensureInitialized();
+  runApp(MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      debugShowCheckedModeBanner: false,
+      title: 'JDIH Kota Madiun',
+      home: AnimatedSplashScreen(
+        splash: 'assets/img/splash.gif',
+        nextScreen: HalamanUtama(),
+        duration: 3000,
+        splashTransition: SplashTransition.fadeTransition,
+        pageTransitionType: PageTransitionType.fade,
+        splashIconSize: double.infinity,
+        backgroundColor: const Color(0xffe5e5e5),
+      ),
+    );
+  }
+}
